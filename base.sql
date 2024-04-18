3f93ea4d357cfc0f28285c1e215e3e86d0a76b29
Ran Ran <rayfarandi1994@gmail.com>
2024-04-19 05:15:51 +0700

    docker, checkpoint

--- base.sql
+++ base.sql
@@ -1,4 +1,5 @@
 -- Active: 1713325435843@@127.0.0.1@5432@postgres
+
 CREATE TABLE "users"(
     "id" SERIAL PRIMARY KEY,
     "full_name" VARCHAR(80),
@@ -7,3 +8,7 @@ CREATE TABLE "users"(
     "created_at" TIMESTAMP DEFAULT NOW(),
     "updated_at" TIMESTAMP
 );
+
+INSERT INTO "users" ("full_name","email","password")
+VALUES ('administartor','admin','123'),
+('guesst','gst','1234');
--- src/lib/db.lib.js
+++ src/lib/db.lib.js
@@ -1,7 +1,10 @@
 const pg = require('pg')
 
 const db = new pg.Pool({
-    connectionString: "postgresql://postgres:1@localhost:5432/monolitic?sslmode=disable"
+    // connectionString: "postgresql://postgres:1@db:5432/monolitic?sslmode=disable"
+    connectionString: "postgresql://postgres:1@db:5432/postgres?sslmode=disable"
+    // docker
+
 })
 
 db.on("connect", ()=>{
